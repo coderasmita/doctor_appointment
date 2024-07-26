786621cac683e864f5d9fa95c21f977801301128
asmita50 <asmitakoirala2022@gmail.com>
2024-07-26 08:15:57 +0545

    primary color change

--- lib/const/app_colors.dart
+++ lib/const/app_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryColor = Color(0xFF1CE16D);
+  static const Color primaryColor = Color(0xFFBBA8F4);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
   static const Color greyColor = Color(0xFF808080);
